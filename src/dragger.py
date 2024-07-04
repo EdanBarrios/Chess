b2a8f5db19a9fa28289e5833df68da620e1d32b0
Edan Barrios <dr.edandre@protonmail.com>
2024-07-04 15:43:31 -0700

    Final Touches

--- src/dragger.py
+++ src/dragger.py
@@ -1,6 +1,6 @@
-from const import *
-
 import pygame
+
+from const import *
 
 
 class Dragger:
@@ -10,31 +10,31 @@ class Dragger:
         self.dragging = False
         self.mouseX = 0
         self.mouseY = 0
-        self.inital_row = 0
-        self.inital_col = 0
+        self.initial_row = 0
+        self.initial_col = 0
 
-    # Blit Methods
+    # blit method
 
     def update_blit(self, surface):
-        # Texture
+        # texture
         self.piece.set_texture(size=128)
         texture = self.piece.texture
-        # Image
+        # img
         img = pygame.image.load(texture)
-        # Rectangle
+        # rect
         img_center = (self.mouseX, self.mouseY)
         self.piece.texture_rect = img.get_rect(center=img_center)
-        # Blit
+        # blit
         surface.blit(img, self.piece.texture_rect)
 
-    # Other methods
+    # other methods
 
     def update_mouse(self, pos):
-        self.mouseX, self.mouseY = pos  # Tuple for coordinates (xcor, ycor)
+        self.mouseX, self.mouseY = pos  # (xcor, ycor)
 
-    def save_inital(self, pos):
-        self.initial_row = pos[1] // SQUARE_SIZE
-        self.initial_col = pos[0] // SQUARE_SIZE
+    def save_initial(self, pos):
+        self.initial_row = pos[1] // SQSIZE
+        self.initial_col = pos[0] // SQSIZE
 
     def drag_piece(self, piece):
         self.piece = piece
--- src/piece.py
+++ src/piece.py
@@ -2,7 +2,7 @@ import os
 
 
 class Piece:
-    # Value is important because the AI will use it to evaluate the board
+
     def __init__(self, name, color, value, texture=None, texture_rect=None):
         self.name = name
         self.color = color
@@ -21,37 +21,46 @@ class Piece:
 
     def add_move(self, move):
         self.moves.append(move)
-        print(
-            f"Move added to {self}: {move.initial.row, move.initial.col} -> {move.final.row, move.final.col}"
-        )
+
+    def clear_moves(self):
+        self.moves = []
 
 
 class Pawn(Piece):
+
     def __init__(self, color):
         self.dir = -1 if color == "white" else 1
+        self.en_passant = False
         super().__init__("pawn", color, 1.0)
 
 
 class Knight(Piece):
+
     def __init__(self, color):
         super().__init__("knight", color, 3.0)
 
 
 class Bishop(Piece):
+
     def __init__(self, color):
         super().__init__("bishop", color, 3.001)
 
 
 class Rook(Piece):
+
     def __init__(self, color):
         super().__init__("rook", color, 5.0)
 
 
 class Queen(Piece):
+
     def __init__(self, color):
         super().__init__("queen", color, 9.0)
 
 
 class King(Piece):
+
     def __init__(self, color):
-        super().__init__("king", color, 100000.0)
+        self.left_rook = None
+        self.right_rook = None
+        super().__init__("king", color, 10000.0)
